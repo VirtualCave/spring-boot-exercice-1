b82f7244d2285d631ff6b1f0ae74bf2a010660f0
rober-cr <42969315+rober-cr@users.noreply.github.com>
2022-02-16 15:20:31 +0100

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -35,22 +35,22 @@ INSERT INTO public.t_rates
 VALUES( 1, 1, '2022-01-01', '2022-05-31', 1550, 'EUR');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
-VALUES( 1, 1, '2022-06-01', '2022-12-31', 1850, 'USC');
+VALUES( 1, 1, '2022-06-01', '2022-12-31', 1850, 'USD');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 2, 1, '2022-01-01', '2022-05-31', 2050, 'EUR');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
-VALUES( 2, 1, '2022-06-01', '2022-12-31', 2250, 'USC');
+VALUES( 2, 1, '2022-06-01', '2022-12-31', 2250, 'USD');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 1, 2, '2022-01-01', '2022-05-31', 2550, 'EUR');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
-VALUES( 1, 2, '2022-06-01', '2022-12-31', 2850, 'USC');
+VALUES( 1, 2, '2022-06-01', '2022-12-31', 2850, 'USD');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 2, 2, '2022-01-01', '2022-05-31', 3050, 'EUR');
 INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
-VALUES( 2, 2, '2022-06-01', '2022-12-31', 3250, 'USC');
+VALUES( 2, 2, '2022-06-01', '2022-12-31', 3250, 'USD');
